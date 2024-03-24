83df6a115d032ea1467b3ae1aedbe80cecf37850
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-24 20:43:17 +0100

    improved geometries

--- lib/components/controller/index.jsx
+++ lib/components/controller/index.jsx
@@ -4,35 +4,34 @@ import { Canvas } from '@react-three/fiber'
 
 
 export function PlayStation() {
+
   return (
     <div className="canvas">
-        <Canvas camera={{ position: [0, 0.2, 3], fov: 55 }}>
-            <directionalLight position-y={-2} />
-            <directionalLight position-y={2} />
-            <directionalLight position-z={1} intensity={0.3} />
-           <group position-y={-1.2} rotation-y={1.6}>
-                <mesh rotation-x={-0.75}>
-                    <boxGeometry args={[0.1, 1, 0.1]} />
-                    <meshStandardMaterial color="blue" />
-                </mesh>
-                <mesh rotation-x={0.75} >
-                    <boxGeometry args={[0.1, 1, 0.1]} />
-                    <meshStandardMaterial color="blue" />
-                </mesh>
-            </group>
-            <mesh position-x={1}>
-                <sphereGeometry args={[0.35, 64, 32]} />
-                <meshStandardMaterial color="blue" />
-            </mesh>
-            <mesh position-x={-1}>
-                <boxGeometry args={[0.6, 0.6, 0.6]} />
-                <meshStandardMaterial color="blue" />
-            </mesh>
-            <mesh position-y={1} rotation-z={-0.5}>
-                <torusGeometry args={[0.4, 0.1, 3, 3, 6.3]} />
-                <meshStandardMaterial color="blue" />
-            </mesh>
-        </Canvas>
+      <Canvas camera={{ position: [0, 0.2, 3], fov: 60 }}>
+        <ambientLight />
+        <group position-y={-1.2} rotation-y={1.6}>
+          <mesh rotation-x={-0.75}>
+            <boxGeometry args={[0.1, 1, 0.1]} />
+            <meshStandardMaterial color="#3f8be8" />
+          </mesh>
+          <mesh rotation-x={0.75} >
+            <boxGeometry args={[0.1, 1, 0.1]} />
+            <meshStandardMaterial color="#3f8be8" />
+          </mesh>
+        </group>
+        <mesh position-x={1}>
+          <torusGeometry args={[0.33, 0.06, 3, 100, 6.3]} />
+          <meshStandardMaterial color="#9c2f22" />
+        </mesh>
+        <mesh position-x={-1} rotation-z={0.78}>
+          <torusGeometry args={[0.4, 0.06, 10, 4, 6.3]} />
+          <meshStandardMaterial color="#bc6ca5" />
+        </mesh>
+        <mesh position-y={1} rotation-z={-0.53}>
+          <torusGeometry args={[0.4, 0.1, 3, 3, 6.3]} />
+          <meshStandardMaterial color="#4ca46d" />
+        </mesh>
+      </Canvas>
     </div>
   )
 }
