ed9ec3e25652f7112ae81a21187da3f026ea463f
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-29 19:52:03 +0100

    updated and new geometries

--- lib/components/spinningCube/index.jsx
+++ lib/components/spinningCube/index.jsx
@@ -1,30 +1,54 @@
 import React, { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
 
 
-export function SpinningCube({ size = 1, color="blue" }) {
+function RotatingCube() {
+  const ref = useRef()
+  const clockRef = useRef(new THREE.Clock())
 
-  function MeshComponent({ size, color }) {
-    const meshRef = useRef()
-    
-    useFrame((state, delta) => {
-      if(meshRef.current) {
-        meshRef.current.rotation.y += delta * 3
-      }
-    })
-    
-    return (
-      <mesh ref={meshRef} rotation={[0, 0, 0]}>
-        <boxGeometry args={[size, size, size]} />
-        <meshStandardMaterial color={color} />
-      </mesh>
-    )
-  }
+  useFrame(() => {
+      const angle = clockRef.current.getElapsedTime() * 2
+      const distance = 2
+      const x = Math.sin(angle) * distance
+      const z = Math.cos(angle) * distance
+      ref.current.position.set(x, 0, z)
+      ref.current.rotation.y += 0.005
+  })
 
   return (
-    <Canvas>
-      <directionalLight position={[-2, 2, 10]} />
-      <MeshComponent size={size} color={color} />
-    </Canvas>
+    <group>
+      <mesh ref={ref}>
+        <sphereGeometry args={[0.15, 128, 128]} />
+        <meshStandardMaterial color="blue" />
+      </mesh>
+    </group>
+  )
+}
+
+export function SpinningCube() {
+  return (
+    <div className="canvas">
+      <Canvas camera={{ position: [0, 0, 6], fov: 55 }}>
+        <ambientLight />
+        <group rotation-z={0.33}>
+          <RotatingCube />
+          <mesh>
+            <sphereGeometry args={[1, 128, 128]} />
+            <meshStandardMaterial color="white" />
+          </mesh>
+          <group rotation-x={0.07}>
+            <mesh rotation-x={1.5} position-y={0.17}>
+              <torusGeometry args={[2.1, 0.02, 20, 100, 6.3]} />
+              <meshStandardMaterial color="red" />
+            </mesh>
+            <mesh rotation-x={1.5} position-y={-0.17}>
+              <torusGeometry args={[2.1, 0.02, 20, 100, 6.3]} />
+              <meshStandardMaterial color="red" />
+            </mesh>
+          </group>
+        </group>
+      </Canvas>
+    </div>
   )
 }
