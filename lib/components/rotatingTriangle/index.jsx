fa95e4a6341e23e17323685d22706a9e6a6e99da
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-02 20:49:05 +0200

    changed geometry

--- lib/components/rotatingTriangle/index.jsx
+++ lib/components/rotatingTriangle/index.jsx
@@ -1,74 +1,46 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import '../main.css'
 import { animated, useSpring } from '@react-spring/three'
+import { Canvas } from '@react-three/fiber'
 
 
-export function SpinTriangle() {
+export function SpinTriangle({ colorLeftBox="blue", colorRightBox="cyan" }) {
 
-    const posY = -0.8
-    const rotZ = -0.54
+  const Triangle = ({ mass = 10, tension= 900, friction = 70, rotateDirection = 1, posXYZ=[0, 0, 0], children }) => {
+    const [step, setStep] = useState(0);
+    const steps = [rotateDirection * -Math.PI / 2, rotateDirection * -Math.PI, rotateDirection * -1.5 * Math.PI, rotateDirection * -2 * Math.PI];
+  
+    const { rotation } = useSpring({
+      reset: true,
+      from: { rotation: step === 0 ? 0 : steps[step - 1] }, 
+      to: { rotation: steps[step] },
+      onRest: () => setStep((step + 1) % steps.length),
+      config: { mass, tension, friction },
+    });
 
-    function TriangleS({ mass = 10, tension= 900, friction = 70 }) {
-      const [step, setStep] = useState(0)
-      const steps = [-Math.PI / 2, -Math.PI, -1.5 * Math.PI, -2 * Math.PI]
-    
-      const { rotation } = useSpring({
-        reset: true,
-        from: { rotation: step === 0 ? 0 : steps[step - 1] }, 
-        to: { rotation: steps[step] },
-        onRest: () => setStep((step + 1) % steps.length),
-        config: { mass, tension, friction },
-      })
-        
-        return (
-          <animated.mesh position-y={posY} rotation-y={rotation} rotation-z={rotZ}>
-            <torusGeometry args={[1, 0.075, 4, 3, 6.3]} />
-            <meshStandardMaterial color="blue" />
-          </animated.mesh>
-        )
-      }
-
-      function TriangleM() {
-        const meshRefM = useRef()
-        useFrame((state, delta) => {
-          if(meshRefM.current) {
-            meshRefM.current.rotation.y -= delta * 1.5
-          }
-        })
-        
-        return (
-          <mesh ref={meshRefM} position-y={posY} rotation-z={rotZ}>
-            <torusGeometry args={[2, 0.075, 4, 3, 6.3]} />
-            <meshStandardMaterial color="cyan" />
-          </mesh>
-        )
-      }
-
-      function TriangleL() {
-        const meshRefL = useRef()
-        useFrame((state, delta) => {
-          if(meshRefL.current) {
-            meshRefL.current.rotation.y += delta
-          }
-        })
-        
-        return (
-          <mesh ref={meshRefL} position-y={posY} rotation-z={rotZ}>
-            <torusGeometry args={[3, 0.075, 4, 3, 6.3]} />
-            <meshStandardMaterial color="blue" />
-          </mesh>
-        )
-      }
+    return (
+      <animated.mesh rotation-x={rotation} position={posXYZ}>
+        {children}
+      </animated.mesh>
+    )
+};
 
   return (
     <div className="canvas">
-    <Canvas>
-        <ambientLight />
-        <TriangleL />
-        <TriangleM />
-        <TriangleS />
-    </Canvas>
+      <Canvas camera={{ position: [-8, 2, 10], fov: 30 }}>
+        <ambientLight intensity={0.75} />
+        <directionalLight intensity={2.5}/>
+        <group position={[0.75, 0, 0]}>
+          <Triangle posXYZ={[-1.5, 0, 0]} rotateDirection={1}>
+            <boxGeometry args={[3, 1, 1]} />
+            <meshStandardMaterial color={colorLeftBox} />
+          </Triangle>
+          <Triangle posXYZ={[1.5, 0, 0]} rotateDirection={-1}>
+            <boxGeometry args={[3, 1, 1]} />
+            <meshStandardMaterial color={colorRightBox} />
+          </Triangle>
+        </group>
+      </Canvas>
     </div>
   )
 }
