fadc0c82f7bdc5882ba85cdfd2912f56a835ae56
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-08 20:59:33 +0200

    Improved animation and props

--- lib/components/controller/index.jsx
+++ lib/components/controller/index.jsx
@@ -1,36 +1,59 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../main.css'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 
 
-export function PlayStation() {
+const RotatingMesh = ({ children, position, rotation, axis }) => {
+  const torusRef = useRef()
+  
+  useFrame(() => {
+    torusRef.current.rotation[axis] += 0.05
+  })
+    
+  return (
+    <mesh position={position} rotation={rotation} ref={torusRef}>
+      { children }
+    </mesh>
+  )
+}
+
+
+export function PlayStation({ colorCross="#3f8be8", colorCircle="#9c2f22", colorCube="#bc6ca5", colorTriangle="#4ca46d"}) {
 
   return (
     <div className="canvas">
-      <Canvas camera={{ position: [0, 0.2, 3], fov: 60 }}>
+      <Canvas camera={{ position: [0, 0.2, 3.1], fov: 60 }}>
         <ambientLight />
-        <group position-y={-1.2} rotation-y={1.6}>
+        {/* Cross */}
+        <RotatingMesh axis="y" position={[0, -1.2, 0]} rotation={[0, 1.6, 0]}>
           <mesh rotation-x={-0.75}>
             <boxGeometry args={[0.1, 1, 0.1]} />
-            <meshStandardMaterial color="#3f8be8" />
+            <meshBasicMaterial color={colorCross} />
           </mesh>
           <mesh rotation-x={0.75} >
             <boxGeometry args={[0.1, 1, 0.1]} />
-            <meshStandardMaterial color="#3f8be8" />
+            <meshBasicMaterial color={colorCross} />
           </mesh>
-        </group>
-        <mesh position-x={1}>
+        </RotatingMesh>
+
+        {/* Circle */}
+        <RotatingMesh axis="x" position={[1, 0, 0]}>
           <torusGeometry args={[0.33, 0.06, 3, 100, 6.3]} />
-          <meshStandardMaterial color="#9c2f22" />
-        </mesh>
-        <mesh position-x={-1} rotation-z={0.78}>
+          <meshBasicMaterial color={colorCircle} />
+        </RotatingMesh>
+
+        {/* Cube */}
+        <RotatingMesh axis="z" position={[-1, 0, 0]} rotation={[0, 0, 0.78]}>
           <torusGeometry args={[0.4, 0.06, 10, 4, 6.3]} />
-          <meshStandardMaterial color="#bc6ca5" />
-        </mesh>
-        <mesh position-y={1} rotation-z={-0.53}>
+          <meshBasicMaterial color={colorCube} />
+        </RotatingMesh>
+
+        {/* Triangle */}
+        <RotatingMesh axis="y" position={[0, 1, 0]} rotation={[0, 0, -0.53]}> 
           <torusGeometry args={[0.4, 0.1, 3, 3, 6.3]} />
-          <meshStandardMaterial color="#4ca46d" />
-        </mesh>
+          <meshStandardMaterial color={colorTriangle} />
+        </RotatingMesh>
+
       </Canvas>
     </div>
   )
