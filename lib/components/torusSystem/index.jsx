32226902cef55a663499b149b3d6196f8a8e7b22
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-17 21:38:47 +0200

    FOV made responsive

--- lib/components/torusSystem/index.jsx
+++ lib/components/torusSystem/index.jsx
@@ -5,14 +5,16 @@ import { Canvas, useFrame } from '@react-three/fiber'
 
 export function TorusSystem({ colorLarge="blue", colorMedium="blue", colorSmall="blue", colorSphere="blue" }) {
 
+  const onMobile = window.innerWidth < 930
+
   const materialMetalness = 2
 
-  const SystemElement = ({ children, speed, axis }) => {
+  const SystemElement = ({ children, speed, axis, operation }) => {
     const meshRef = useRef()
     
     useFrame((state, delta) => {
       if(meshRef.current) {
-        meshRef.current.rotation[axis] += delta * speed
+        meshRef.current.rotation[axis] = operation(meshRef.current.rotation[axis], delta * speed)
       }
     })
     return(
@@ -24,24 +26,24 @@ export function TorusSystem({ colorLarge="blue", colorMedium="blue", colorSmall=
 
   return (
     <div className='canvas'>
-      <Canvas camera={{ position: [-5, 5, 5], fov: 55 }}>
+      <Canvas camera={{ position: [-5, 5, 5], fov:(onMobile ? 95 : 55) }}>
         <directionalLight intensity={2} />
         <ambientLight intensity={0.3} />
         {/* Toruse Large */}
-        <SystemElement axis="x" speed={1}>
-          <torusGeometry args={[3, 0.2, 5, 100, 6.3]} />
+        <SystemElement axis="x" speed={1} operation={(a, b) => a + b}>
+          <torusGeometry args={[3, 0.2, 20, 100, 6.3]} />
           <meshStandardMaterial color={colorLarge} metalness={materialMetalness} />
         </SystemElement>
 
         {/* Toruse Medium */}
-        <SystemElement axis="y" speed={1.5}>
-          <torusGeometry args={[2, 0.2, 5, 100, 4.5]} />
+        <SystemElement axis="y" speed={1.5} operation={(a, b) => a + b}>
+          <torusGeometry args={[2, 0.2, 20, 100, 6.3]} />
           <meshStandardMaterial color={colorMedium} metalness={materialMetalness} />
         </SystemElement>
 
         {/* Toruse Small */}
-        <SystemElement axis="z" speed={3}>
-          <torusGeometry args={[1, 0.2, 5, 100, 3.5]} />
+        <SystemElement axis="x" speed={3.5} operation={(a, b) => a - b}>
+          <torusGeometry args={[1, 0.2, 20, 100, 6.3]} />
           <meshStandardMaterial color={colorSmall} metalness={materialMetalness} />
         </SystemElement>
 
