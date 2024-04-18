f31d8e047432f71df12bc84567fc11b3b63cea86
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-18 20:51:26 +0200

    Updated fov and export name

--- lib/components/spinningCube/index.jsx
+++ lib/components/spinningCube/index.jsx
@@ -3,7 +3,7 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 
 
-function RotatingCube() {
+function RotatingSphere() {
   const ref = useRef()
   const clockRef = useRef(new THREE.Clock())
 
@@ -20,31 +20,43 @@ function RotatingCube() {
     <group>
       <mesh ref={ref}>
         <sphereGeometry args={[0.15, 128, 128]} />
-        <meshStandardMaterial color="blue" />
+        <meshPhysicalMaterial color= "silver"
+          roughness= {0.2}
+          metalness= {1.0}
+          clearcoat= {1.0}
+          clearcoatRoughness= {0.1 }/>
       </mesh>
     </group>
   )
 }
 
-export function SpinningCube() {
+export function DarkPlanet({ colorRing="blue", colorCenterSphere="blue" }) {
+
+  const onMobile = window.innerWidth < 930
+
   return (
     <div className="canvas">
-      <Canvas camera={{ position: [0, 0, 6], fov: 55 }}>
+      <Canvas camera={{ position: [0, 0, 6], fov:(onMobile ? 85 : 55) }}>
+        <directionalLight />
         <ambientLight />
         <group rotation-z={0.33}>
-          <RotatingCube />
+          <RotatingSphere />
           <mesh>
             <sphereGeometry args={[1, 128, 128]} />
-            <meshStandardMaterial color="white" />
+            <meshStandardMaterial color={colorCenterSphere} />
           </mesh>
           <group rotation-x={0.07}>
             <mesh rotation-x={1.5} position-y={0.17}>
               <torusGeometry args={[2.1, 0.02, 20, 100, 6.3]} />
-              <meshStandardMaterial color="red" />
+              <meshStandardMaterial 
+                color={colorRing} 
+              />
             </mesh>
             <mesh rotation-x={1.5} position-y={-0.17}>
               <torusGeometry args={[2.1, 0.02, 20, 100, 6.3]} />
-              <meshStandardMaterial color="red" />
+              <meshStandardMaterial 
+                color={colorRing} 
+              />
             </mesh>
           </group>
         </group>
